0983462ce4560f141058dddb58f7088148d35de7
Marcelo Carlos <marcelocarlosjr@hotmail.com>
2022-03-14 20:03:09 -0400

    Player controller
    Finished Player controller, building maze.
    
    TODO:
    -NAVMESH patrol maze, chase when .7 units away.
    -Bake NAVMESH surface

--- Assets/Scripts/RigidBodyController.cs
+++ Assets/Scripts/RigidBodyController.cs
@@ -7,22 +7,53 @@ using NETWORK_ENGINE;
 public class RigidBodyController : NetworkComponent
 {
     public Rigidbody MyRig;
+    public float speed = 3;
+    public float rotationSpeed;
+    public Vector3 LastMove;
+
+    public bool vZero;
+    public bool hZero;
+
+    public float v;
+
+    public float h;
+
+
     public override void HandleMessage(string flag, string value)
     {
-        
+        if(flag == "MOVE" && IsServer)
+        {
+            string[] args = value.Split(',');
+            float h = float.Parse(args[0]);
+            float v = float.Parse(args[1]);
+            LastMove = new Vector3(h, 0, v);
+        }
     }
 
     public override void NetworkedStart()
     {
-        
+        if (IsServer)
+        {
+            this.transform.position = new Vector3(0, 1.1f, -3);
+        }
     }
 
     public override IEnumerator SlowUpdate()
     {
-        while (IsServer)
+        while (IsConnected)
         {
-            MyRig.velocity = this.transform.forward * 15;
-            MyRig.angularVelocity = new Vector3(0, Mathf.PI / 2, 0);
+            if (IsLocalPlayer)
+            {
+                if (new Vector3(v, 0, h).magnitude > 0.1f)
+                {
+                    LastMove = new Vector3(h, 0, v);
+                    SendCommand("MOVE", h + "," + v);
+                }
+                else if(LastMove.magnitude == 0)
+                {
+                    SendCommand("MOVE", 0 + "," + 0);
+                }
+            }
             yield return new WaitForSeconds(0.05f);
         }
     }
@@ -40,6 +71,21 @@ public class RigidBodyController : NetworkComponent
     
     void Update()
     {
-        
+        if (IsServer)
+        {
+            if(LastMove.z > 0.1 || LastMove.z < -0.1)
+            {
+                MyRig.velocity = this.transform.forward * LastMove.z * speed;
+            }
+            if(LastMove.x != 0)
+            {
+                MyRig.angularVelocity = new Vector3(0, LastMove.x * rotationSpeed, 0);
+            }
+        }
+        if (IsClient)
+        {
+            h = Input.GetAxis("Horizontal");
+            v = Input.GetAxis("Vertical");
+        }
     }
 }
